866f9713c00e4cc46dab5cded23030c5b8906546
DINESH-052004 <dineshrr052004@gmail.com>
2026-02-28 17:33:58 +0530

    updated login page

--- pratice1.py
+++ pratice1.py
@@ -8,8 +8,17 @@
 
 # given input alphabet or not
 
-alpha=input("enter your char")
-if alpha>='a' and alpha<='z':
-    print(alpha,"alphabet")
+# alpha=input("enter your char")
+# if alpha>='a' and alpha<='z':
+#     print(alpha,"alphabet")
+# else:
+#     print("not")
+
+# vowel or constant
+vc=input("enter your char")
+if vc=='a' or vc=="e" or vc=="i" or vc=="o" or vc=="u":
+    print("vowel",vc)
+elif vc=="A" or vc=="E" or vc=="I" or vc=="O" or vc=="U":
+    print("vowel",vc)
 else:
-    print("not")
+    print("given constant ")
