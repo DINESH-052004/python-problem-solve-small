c78fbd7de7bcd7e5860da85565912481d2b1a821
DINESH-052004 <dineshrr052004@gmail.com>
2026-02-28 18:53:26 +0530

    updated login page

--- pratice1.py
+++ pratice1.py
@@ -15,10 +15,25 @@
 #     print("not")
 
 # vowel or constant
-vc=input("enter your char")
-if vc=='a' or vc=="e" or vc=="i" or vc=="o" or vc=="u":
-    print("vowel",vc)
-elif vc=="A" or vc=="E" or vc=="I" or vc=="O" or vc=="U":
-    print("vowel",vc)
-else:
-    print("given constant ")
+# vc=input("enter your char")
+# if vc=='a' or vc=="e" or vc=="i" or vc=="o" or vc=="u":
+#     print("vowel",vc)
+# elif vc=="A" or vc=="E" or vc=="I" or vc=="O" or vc=="U":
+#     print("vowel",vc)
+# else:
+#     print("given constant ") 
+
+# averagenum
+
+# a=int(input("enter your num"))
+# b=int(input("enter 2 num"))
+# c=int(input("enter 3 num"))
+# avg=(a+b+c)/3
+# print(avg)
+
+a=int(input("enter num"))
+avg=0
+for i in range(1,a+1):
+    avg=avg+i
+c=avg/a
+print(c)
