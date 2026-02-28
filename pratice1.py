7a5e57c779f77466bf8e154cc8fd9eba13f8785e
DINESH-052004 <dineshrr052004@gmail.com>
2026-02-28 22:56:58 +0530

    updated login page

--- pratice1.py
+++ pratice1.py
@@ -31,9 +31,139 @@
 # avg=(a+b+c)/3
 # print(avg)
 
-a=int(input("enter num"))
-avg=0
-for i in range(1,a+1):
-    avg=avg+i
-c=avg/a
-print(c)
+# a=int(input("enter num"))
+# avg=0
+# for i in range(1,a+1):
+#     avg=avg+i
+# c=avg/a
+# print(c)
+
+# divisible by given number
+
+# st=10
+# end=20
+# number=2
+# for i in range(st,end+1):
+#     if i%number==0:
+#         print("divisible",i)
+
+# swapping temp veriable
+
+# a=10
+# b=20
+# print(a,b)
+# c=a
+# a=b
+# b=c
+# print(a,b)
+
+# without temp veriable
+
+# a=10
+# b=20
+# a,b=20,10
+# print(a,b)
+
+# find quient and remainder
+
+# a=int(input("enter num1"))
+# b=int(input("enter num2"))
+# quient=a/b
+# remainder=a%b
+# print(quient)
+# print(remainder)
+
+# find odd number
+
+# a=int(input("enter num1"))
+# b=int(input("enter num2"))
+# for i in range(a,b+1):
+#     if i%2!=0:
+#         print(i)
+    
+# find even num
+
+
+# a=int(input("enter num1"))
+# b=int(input("enter num2"))
+# for i in range(a,b+1):
+#     if i%2==0:
+#         print(i)
+
+# multiplication table
+
+# table=int(input("enter table"))
+# st=int(input("enter st num"))
+# end=int(input("enter end num"))
+# for i in range(st,end+1):
+#     a=i*table
+#     print(i,"x",table,"=",a)
+
+# unit conversion
+
+# 1km=0.62137miles
+
+# a=int(input("enter your km"))
+# miles=a*0.62137
+# print(a,"to",miles)
+
+# miles to km
+
+# a=int(input("enter your miles"))
+# km=a/0.62137
+# print(a,"to",km)
+
+# celcius to feharenhit
+
+# cel=float(input("enter your celcius"))
+# fah=(cel*1.8)+32 //9/5
+# print(fah)
+
+# fah to cel
+
+# fah=float(input("enter your fah"))
+# cel=(fah-32)*5/9
+# print(cel)
+
+# find area of triangle
+
+# a=5
+# b=6
+# c=7
+# s=(a+b+c)/2
+# area=(s*(s-a)*(s-b)*(s-c))**0.5
+# print(area)
+
+# largest number amount 3 number
+
+# a=10
+# b=20
+# c=21
+# if a>=b and a>=c:
+#     large=a
+# elif b>=a and b>=c:
+#     large=b
+# elif c>=a and c>=b:
+#     large=c
+#     print(large)
+    
+# num=int(input("enter num"))
+# if num<0:
+#     print("negative")
+# else:
+#     val=0
+#     while(num>0):
+#         val=val+num
+#         num=num-1
+#     print(val)
+
+num=int(input("enter factnum"))
+val=1
+if num<0:
+    print("enter positive")
+else:
+    
+    while(num>=1):
+        val=val*num
+        num=num-1
+    print(val)
