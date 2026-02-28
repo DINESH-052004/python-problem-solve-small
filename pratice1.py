69353d549bde39815a49f63d513d0f29d65129f5
DINESH-052004 <dineshrr052004@gmail.com>
2026-02-28 17:18:21 +0530

    updated login page

--- pratice1.py
+++ pratice1.py
@@ -1,5 +1,7 @@
 a=int(input("enter your num"))
-if a<0:
-    print("negative")
-elif a>=0:
-    print("positive")
+if a%2==0:
+    print("even")
+elif a%2!=0:
+    print("odd")
+else:
+    print("0")
